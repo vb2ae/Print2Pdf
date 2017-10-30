157e17ec4f4af2c56b31da13ea16f5ac909b9916
Ken Tucker <vb2ae@live.com>
2017-10-30 10:12:00 -0400

    Updated to .net 4.5.2

--- WindowsApplication1/Form1.vb
+++ WindowsApplication1/Form1.vb
@@ -8,7 +8,7 @@ Public Class Form1
     Dim ds As New DataSet
 
     Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
-        If PrintDialog1.ShowDialog() = Windows.Forms.DialogResult.OK Then
+        If PrintDialog1.ShowDialog() = DialogResult.OK Then
             If PrintDialog1.PrinterSettings.PrintToFile = True Then
                 With PdfPrinter1
                     .Title = "Product Catalog"
